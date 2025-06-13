79699a1a108095500bdd6efa0dae50de1c10833a
neera <lotusmohanoct28@gmail.com>
2025-06-14 11:04:06 +1200

    practise git giles

--- src/ex_01_Java_Basic/Lab003_HW3.java
+++ src/ex_01_Java_Basic/Lab003_HW3.java
@@ -3,7 +3,7 @@ package ex_01_Java_Basic;
 public class Lab003_HW3 {
     public static void main(String[] args) {
         System.out.println("Hi Neeru");
-        // Testing Git commit practice3
+        // Testing Git commit practice1
 
     }
 }
